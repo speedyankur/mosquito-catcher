5a7ebf2b43cd03563c60ba5e32907c919a4e68e0
Ankur Garha <A65379@eon.com>
2022-07-08 13:03:16 +0200

    Added background image and removed the default ball

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -1,26 +1,19 @@
 export default function sketch(s) {
-  let x, y, backgroundColor;
+  let bg;
 
-  const width = 500;
-  const height = 500;
+  const width = 1400;
+  const height = 700;
 
   s.setup = () => {
     s.createCanvas(width, height);
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
-
-    x = s.random(width);
-    y = height / 2;
+    bg = s.loadImage("assets/bg.jpg");
   };
 
   s.draw = () => {
-    s.background(backgroundColor);
-    s.fill(s.color(255, 0, 0));
-    s.ellipse(x, y, 100, 100);
-
-    x = (x + 1) % width;
+    s.background(bg);
   };
 
   s.mousePressed = () => {
-    backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
+    //backgroundColor = s.color(s.random(255), s.random(255), s.random(255));
   };
 }
